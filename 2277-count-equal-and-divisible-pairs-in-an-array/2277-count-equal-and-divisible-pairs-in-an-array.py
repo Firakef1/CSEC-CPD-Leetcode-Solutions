d9955afe23058ddae489a750e94e9ce87ee440ad
Firakef1 <firaolkef@gmail.com>
2025-03-20 16:18:58 +0300

    Time: 371 ms (5.74%), Space: 12.4 MB (85.25%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.py
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.py
@@ -10,15 +10,42 @@ class Solution(object):
         length = len(nums)
         counter = 0
         print(nums)
+        # for i in range(length):
+
+        #     for j in range(i+1, length):
+
+        #         if (i*j)%k == 0 and nums[i] == nums[j]:
+
+        #             counter += 1
+
+        #             print(i, j)
+
+
+        store_dict = {}
+
         for i in range(length):
 
-            for j in range(i+1, length):
+            if nums[i] not in store_dict:
 
-                if (i*j)%k == 0 and nums[i] == nums[j]:
+                store_dict[nums[i]] = [i]
 
-                    counter += 1
+            else:
 
-                    print(i, j)
+                store_dict[nums[i]].append(i)
+
+        for i in store_dict.keys():
+
+            if len(store_dict[i]) > 1:
+
+                for j in range(len(store_dict[i])):
+
+                    for z in range(j+1, len(store_dict[i])):
+
+                        if (store_dict[i][j]*store_dict[i][z])%k == 0:
+
+                            counter +=1
+                            print(j, z)
+  
 
         return counter 
         
